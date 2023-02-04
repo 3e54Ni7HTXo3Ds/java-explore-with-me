3eab033302af586480d8ef1a568247e0ec9d081b
3e54Ni7HTXo3Ds <mspisarev@yandex.com>
2023-02-04 13:00:42 +0300

    fix: post hit checkstyle

--- stats/stats-server/src/main/java/ru/practicum/ewm/stats/controller/StatsController.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/stats/controller/StatsController.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.ewm.stats.dto.HitRequestDto;
-import ru.practicum.ewm.stats.dto.HitResponseDto;
 import ru.practicum.ewm.stats.service.StatsService;
 
 import javax.validation.Valid;
@@ -21,7 +20,7 @@ public class StatsController {
     private final StatsService statsService;
 
     @PostMapping("/hit")
-    @ResponseStatus (HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.CREATED)
     public void createHit(
             @Valid @RequestBody HitRequestDto hitRequestDto) {
         log.info("Creating hit in stats service");
--- stats/stats-server/src/main/java/ru/practicum/ewm/stats/service/StatsService.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/stats/service/StatsService.java
@@ -1,7 +1,6 @@
 package ru.practicum.ewm.stats.service;
 
 import ru.practicum.ewm.stats.dto.HitRequestDto;
-import ru.practicum.ewm.stats.dto.HitResponseDto;
 
 public interface StatsService {
     void create(HitRequestDto hitRequestDto);
