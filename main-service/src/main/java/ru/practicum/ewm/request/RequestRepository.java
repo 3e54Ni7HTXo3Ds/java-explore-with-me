d9344ae69863f1646e73c4964f1d09c89a2aff76
3e54Ni7HTXo3Ds <mspisarev@yandex.com>
2023-02-13 00:12:25 +0300

    fix: compilations start

--- main-service/src/main/java/ru/practicum/ewm/request/RequestRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/request/RequestRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 import ru.practicum.ewm.request.model.Request;
 
 import java.util.List;
+
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Long> {
     List<Request> getAllByEventIdAndStatus(Long eventId, String status);
